44d2acdb0ee02cf754217da728483b3a2b7fa961
hema11920 <kasireddygarihemasree@gmail.com>
2024-12-14 09:49:38 +0530

    Add files via upload

---
+++ treemap.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class treemap{
+    public static void main(String args[]){
+        TreeMap<String,Integer> a=new TreeMap<>();
+        System.out.println(a);
+    }
+}        
+
---
+++ treemapwithdata.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class treemapwithdata{
+    public static void main(String args[]){
+        TreeMap<String,String> a=new TreeMap<>();
+        a.put("SNO","01");
+        a.put("SName","Yasmin");
+        System.out.println(a);
+    }
+}        
